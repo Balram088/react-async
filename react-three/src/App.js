ab7ceba5eadfb252eace6ada4abceb9e48196072
Balram Patidar <pbalram088@gmail.com>
2022-06-23 22:06:12 +0530

    fb past part
    fb post part

--- react-three/src/App.js
+++ react-three/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { PostMsg } from './Post/Post';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <PostMsg 
+         img="https://avatars.githubusercontent.com/u/93612939?v=4"
+         title="Balram Patidar"
+         posts={["Hello guys", "Good Morning","Good Afternoon","Good Night"]}
+      />
+
+      <PostMsg/>
+
+  
     </div>
   );
 }
