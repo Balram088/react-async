bb74f2662a865390ea41cd4a07b59278b98282be
Balram Patidar <pbalram088@gmail.com>
2022-06-25 21:15:02 +0530

    first todo
    first todo

--- react-three/src/App.js
+++ react-three/src/App.js
@@ -1,21 +1,22 @@
 
 import './App.css';
-import { PostMsg } from './Post/Post';
+import { PostMsg } from './sprint1/Post/Post';
+import FirstTodo from './sprint1/Todo/FirstTodo';
 
 
 
 function App() {
   return (
     <div className="App">
+       <FirstTodo/>
        <PostMsg 
          img="https://avatars.githubusercontent.com/u/93612939?v=4"
          title="Balram Patidar"
          posts={["Hello guys", "Good Morning","Good Afternoon","Good Night"]}
       />
-
-      <PostMsg/>
-
-  
+     {/* <PostMsg/>  usewithout props and use destructuring method */}
+    
+   
     </div>
   );
 }
---
+++ react-three/src/sprint1/Todo/FirstTodo.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function FirstTodo() {
+  const [text, setText] = useState("");
+  const [todos, addTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleClear = () => {
+    setText("");
+  };
+  const handleAdd = () => {
+    addTodos([...todos, text]);
+    handleClear();
+  };
+
+  return (
+    <div className="App">
+      
+    <h1>First Todo</h1>
+      <input value={text} type="text" onChange={handleChange}  placeholder="Add Item" />
+      <br />
+      <button onClick={handleClear}>Clear Text</button>
+      <button onClick={handleAdd}>Add Item</button>
+      <h3>Text:{text}</h3>
+      <h4>Total Task-- {todos.length}</h4>
+      <ol>
+        {todos.map((todo) => (
+          <li> {todo} </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
