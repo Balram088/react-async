a25030593091c07edcfe81c7f1641c78b0fb9d33
Balram Patidar <pbalram088@gmail.com>
2022-07-07 00:42:35 +0530

    open user profile with router
    open user profile with router

--- react-three/src/sprint3/Router/Components/UserPage.jsx
+++ react-three/src/sprint3/Router/Components/UserPage.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 
 
 const UserPage = () => {
+  const [data,setData] =useState({});
+
   const params = useParams();
   console.log(params);
-
-
+  useEffect(() => {
+    fetch(` https://reqres.in/api/users/${params.id}`)
+    .then((res) => res.json())
+    .then((res) =>{
+     setData(res.data)
+    })
+ }, []);
+console.log("fgasfafa",data);
   return (
     <div>
-      <h1>User singal page</h1>
+        <h1>User singal page</h1>
     <h3>User{params.id}</h3>
     <div>
-      <Link to="/users">Go Back</Link>
+    
+      <div>
+        <h1>User</h1>
+        <h3>Name: {data.first_name}</h3>
+        <img width="300px" src={data.avatar} alt={data.id} />
+        <div> <Link to="/users">Go Back</Link>  </div>
+    </div>
     </div>
     </div>
   )
--- react-three/src/sprint3/Router/Components/Users.jsx
+++ react-three/src/sprint3/Router/Components/Users.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-let ids=[1,2,3,4,5]
+// let ids=[1,2,3,4,5]
+
 
 export const Users = () => {
+ const [data,setData] =useState([]);
+  useEffect(() => {
+     fetch(`https://reqres.in/api/users`)
+     .then((res) => res.json())
+     .then((res) =>{
+      setData(res.data)
+     })
+  },[]);
   return (
     <div>
-        {ids.map((e)=>(
-      <h3 key={e}>
-      
-        <Link to={`/users/${e}`}>User{e}</Link>
+        {data.map((user)=>(
+      <div key={user.id} >
+        <h3>{user.first_name}</h3>
+        <img width="300px" src={user.avatar} alt={user.id} />
+        <br />
+        <Link to={`/users/${user.id}`}>More..</Link>
+       
         
-      </h3>
-  
+      </div>
+     
       ))}
-   
+  
     </div>
   )
 }
